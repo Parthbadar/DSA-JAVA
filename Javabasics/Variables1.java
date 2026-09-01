6bd66619cd2495a3d1a55017977230e2a891c897
Parthbadar <parthbadar9@gmail.com>
2026-09-01 22:08:09 +0530

    Add variable Program

---
+++ Javabasics/Variables1.java
@@ -0,0 +1,19 @@
+public class Variables1 {
+    public static void main(String args[]) {
+        int a=10;
+        int b=5;
+        String name = "parth";
+        char ch = 'A';              //Data type: char,int,float,boolen etc.  
+       
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        System.out.println(ch);
+
+        a = b;
+        System.out.println(a);
+
+
+    }
+}
